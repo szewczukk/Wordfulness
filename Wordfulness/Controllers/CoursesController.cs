f8ec17ddb8a3ad961014c512de561d04f8af296b
szewczukk <dmitro.szewczuk@gmail.com>
2023-02-16 21:33:08 +0100

    Add deletion pages

--- Wordfulness/Controllers/CoursesController.cs
+++ Wordfulness/Controllers/CoursesController.cs
@@ -33,6 +33,35 @@ namespace Wordfulness.Controllers
 			return View(new CoursesDetailsViewModel { Course = course });
 		}
 
+		public async Task<IActionResult> Delete(int? id)
+		{
+			var course = await _context.Courses.FirstOrDefaultAsync(course => course.Id == id);
+
+			if (course == null)
+			{
+				return NotFound();
+			}
+
+			return View(course);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var course = await _context.Courses.FindAsync(id);
+
+			if (course == null)
+			{
+				return NotFound();
+			}
+
+			 _context.Courses.Remove(course);
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction(nameof(Index));
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
