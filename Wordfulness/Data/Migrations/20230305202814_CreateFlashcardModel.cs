0162e82395d175f01e37f888b3fbe21fe0582680
szewczukk <dmitro.szewczuk@gmail.com>
2023-03-05 22:16:48 +0100

    Display parents name in AllLessonsView and AllFlashcardsView

--- Wordfulness/Data/Migrations/20230305202814_CreateFlashcardModel.cs
+++ Wordfulness/Data/Migrations/20230305202814_CreateFlashcardModel.cs
@@ -4,42 +4,42 @@
 
 namespace Wordfulness.Migrations
 {
-    public partial class CreateFlashcardModel : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Flashcard",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Front = table.Column<string>(type: "TEXT", nullable: false),
-                    Back = table.Column<string>(type: "TEXT", nullable: false),
-                    CourseId = table.Column<int>(type: "INTEGER", nullable: false),
-                    LessonId = table.Column<int>(type: "INTEGER", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Flashcard", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Flashcard_Lessons_CourseId",
-                        column: x => x.CourseId,
-                        principalTable: "Lessons",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+	public partial class CreateFlashcardModel : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.CreateTable(
+				name: "Flashcard",
+				columns: table => new
+				{
+					Id = table.Column<int>(type: "INTEGER", nullable: false)
+						.Annotation("Sqlite:Autoincrement", true),
+					Front = table.Column<string>(type: "TEXT", nullable: false),
+					Back = table.Column<string>(type: "TEXT", nullable: false),
+					CourseId = table.Column<int>(type: "INTEGER", nullable: false),
+					LessonId = table.Column<int>(type: "INTEGER", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Flashcard", x => x.Id);
+					table.ForeignKey(
+						name: "FK_Flashcard_Lessons_CourseId",
+						column: x => x.CourseId,
+						principalTable: "Lessons",
+						principalColumn: "Id",
+						onDelete: ReferentialAction.Cascade);
+				});
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Flashcard_CourseId",
-                table: "Flashcard",
-                column: "CourseId");
-        }
+			migrationBuilder.CreateIndex(
+				name: "IX_Flashcard_CourseId",
+				table: "Flashcard",
+				column: "CourseId");
+		}
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Flashcard");
-        }
-    }
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropTable(
+				name: "Flashcard");
+		}
+	}
 }
